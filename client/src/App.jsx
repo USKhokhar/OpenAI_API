6927e5496823e106bc2c3d7a6ec8e92b72ca2d59
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2023-02-25 10:02:27 +0530

    client-server-integrated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 const App = () => {
 
-    const handleSubmit = (e) => {
+    const [ input, setInput ] = useState('')
+    const [response, setResponse] = useState('')
+    const [isVisible, setIsVisible] = useState(false)
+
+    const handleSubmit = async (e) => {
         e.preventDefault()
+        setInput('')
+
+    const res = await fetch('http://localhost:5000/', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: input
+        })
+    })
+
+    if(res.ok){
+        const data = await res.json()
+        
+        setResponse(data.bot)
+    } else{
+        const err = await res.text()
+
+        alert(`Something Went Wrong: ${err}`)
     }
+
+    setIsVisible(true)
+}
+
+    const handleKey = (e) => {
+        if(e.key === 'ENTER'){
+            console.log(`object`)
+        }
+    }   
 
     return (
         <main>
             <h1>GPT</h1>
+            
+            {
+                isVisible ? (
+                    <div className='human'>
+                        <span className="icon">
+                            😃 
+                        </span>
+
+                        <p className="user-text">{response}</p>
+                    </div>
+                ) : null
+            }
+
             <form onSubmit={handleSubmit}>
-                <textarea name="input" id="input" cols="1" rows="1" placeholder='Enter Your Query'></textarea>
-                <button type="submit">
-                    ➡ 
-                </button>
+                <input type={'text'} onKeyUp={handleKey} value={input} onChange={(e) => setInput(e.target.value)} name="input" id="input" placeholder='Enter Your Query'></input>
+                <button type="submit"> ➡ </button>
             </form>
         </main>
     )
--- server/server.js
+++ server/server.js
@@ -26,16 +26,16 @@ app.post('/', async (req, res) => {
     try {
         const prompt = req.body.prompt
 
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "\"\"\"\nUtil exposes the following:\nutil.openai() -> authenticates & returns the openai module, which has the following functions:\nopenai.Completion.create(\n    prompt=\"<my prompt>\", # The prompt to start completing from\n    max_tokens=123, # The max number of tokens to generate\n    temperature=1.0 # A measure of randomness\n    echo=True, # Whether to return the prompt in addition to the generated completion\n)\n\"\"\"\nimport util\n\"\"\"\nCreate an OpenAI completion starting from the prompt \"Once upon an AI\", no more than 5 tokens. Does not include the prompt.\n\"\"\"\n",
-        temperature: 0.7,
-        max_tokens: 64,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-        stop: ["\"\"\""],
-    })
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0.7,
+            max_tokens: 64,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+            stop: ["\"\"\""],
+        })
 
     res.status(200).send({
         bot: response.data.choices[0].text
